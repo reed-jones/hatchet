f6b0da086ef8c5907c605dc1edde11036e7189a3
Reed Jones <reed.jones@pderas.com>
2019-08-28 08:50:30 -0600

    cleanup for v1.0.0-alpha

--- hatchet.js
+++ hatchet.js
@@ -13,8 +13,6 @@ const { meow } = require('./meow')
 // load the configuration file
 const configuration = require(meow.flags.config)
 
-// TODO: merge the configuration above with default options
-
 const config = {
     // Pass initialized clients here.
     // Notification Clients must provide a 'sendMessage(<string> message, <object> details)'
--- meow.js
+++ meow.js
@@ -1,27 +1,32 @@
 
 // Meow is the CLI helper
 const meow = require('meow')
+const pkg = require('./package.json')
 
 // get users homedir for config finding
 const homedir = require('os').homedir()
 
 // Configure 'meow' cli client
 module.exports.meow = meow(`
+    Version: ${pkg.version}
+
     Usage
       $ hatchet
 
     Options
-      --config, -c  Path to configuration file
+      --help,     -h  Display Help
+      --version,  -v  Display version information
+      --config,   -c  Path to configuration file
 
     Examples
       $ hatchet --config /path/to/config.js
 `, {
     flags: {
-        config: {
-            type: 'string',
-            alias: 'c',
-            // defaults to home dir
-            default: `${homedir}/.hatchetrc.js`
-        }
+      config: {
+          type: 'string',
+          alias: 'c',
+          // defaults to home dir
+          default: `${homedir}/.hatchetrc.js`
+      }
     }
 });
